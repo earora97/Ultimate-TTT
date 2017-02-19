6b2cc05524a4f162f04e023b3bf10a6e7ea79917
earora97 <earora97@gmail.com>
2017-02-19 16:13:39 +0530

    .

--- code.py
+++ code.py
@@ -13,38 +13,48 @@ class My_Player():
 		else:
 			return b
 	def move(self, board, old_move, flag):
-		print "Old Move:"
-		print old_move
+		print "Old Move:" , old_move
 		self.cn=0
-		next_move = self.minimax(old_move, False, 1000,-1000,board,flag)
+		value,next_move = self.minimax(old_move, False, 1000,-1000,board)
 		return next_move
 	
-	def minimax(self,old_move, maximizingPlayer, alpha, beta,board,flag):
+	def minimax(self,old_move, maximizingPlayer, alpha, beta,board):
 		#did u win the game ,if yes it is a leaf node
+		board.print_board()
+		if(maximizingPlayer):
+			flag = 'x'
+		else:
+			flag = 'o'
+		
 		hvalue	= self.check_win(board,flag)
-		print hvalue
+		print "Hvalue:",hvalue
 		if(hvalue == 'o'):
-			return 10
+			print "Hiiiiii:",best_move
+			return 10,None
 		elif(hvalue == 'x'):
-			return -10
-		elif(hvalue == 'd'):
-			return 0
+			return -10 , None
+		elif(hvalue == 'NONE'):
+			return 0, None
+
 		cells = board.find_valid_move_cells(old_move)	
-		print "Valid moves:"
-		print cells
+	#	print "Valid moves:"
+	#	print cells
 		for mycell in cells:
-			best_move = (-1,-1)
-			myblock = [mycell[0]%4, mycell[0]%4]	
+			best_move = None
+			myblock = [mycell[0]//4, mycell[1]//4]	
 			board.board_status[mycell[0]][mycell[1]] = flag
 			#did u win the block u placed the flag in
 			#if yes change block_status
 			winlose = self.check_block(board,flag,myblock)
-			print winlose
+			print "Winlose:", winlose
 			if(winlose):
 				board.block_status[myblock[0]][myblock[1]] = flag
 			if(maximizingPlayer):
-				best = -1000;
-				val, new_move = self.minimax(mycell, False ,alpha,beta,board,flag)
+				best = -1000
+				
+				val, new_move = self.minimax(mycell, False ,alpha,beta,board)
+				
+				
 				board.board_status[mycell[0]][mycell[1]] = '-'
 				if best < val:
 					best = val
@@ -52,36 +62,37 @@ class My_Player():
 				alpha = self.max(alpha,best)
 				if(beta<=alpha):
 					break
-				return best, best_move
+				return best,best_move
 			else:
-				best = 1000;
-				val, new_move = self.minimax(mycell,True ,alpha,beta,board,flag)
+				best = 1000
+				val, new_move = self.minimax(mycell,True ,alpha,beta,board)
 				board.board_status[mycell[0]][mycell[1]] = '-'
 				if best > val:
 					best = val
 					best_move=mycell
-				alpha = self.min(alpha,best)
+				beta = self.min(alpha,best)
 				if(beta<=alpha):
 					break
-				return best, best_move			
+				return best, best_move
 
 	def check_win(self,board,flag):
 		self.cn+=1
 		if self.cn>30:
-			return True
+			return 'o'
 		whowonorlost = board.find_terminal_state()
-		return whowonorlost[1]
+		return whowonorlost[0]
 
 	def check_block(self,board,flag,myblock):
-		print "ho there"
 		bs = board.board_status
 		a = 4*myblock[0]
 		b = 4*myblock[1]
+
 		#diagonals
 		if(bs[a][b] ==  bs[a+1][b+1] == bs[a+2][b+2] ==bs[a+3][b+3] == flag):
 			return True
 		elif(bs[a][b+3] == bs[a+1][b+2] == bs[a+2][b+1] ==bs[a+3][b] == flag):
 			return True
+		
 		#columns
 		elif(bs[a][b] == bs[a][b+1] == bs[a][b+2] ==bs[a][b+3] == flag):
 			return True
